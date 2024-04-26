ad8c3385ad5d97aff9663e1ef91d29b7837bf807
vdircio <vdirci2@illinois.edu>
2024-04-26 15:47:01 -0500

    up and running

--- Client/src/components/SearchBar.jsx
+++ Client/src/components/SearchBar.jsx
@@ -17,7 +17,7 @@ export const SearchBar = ({ setResults }) => {
     };
 
     const search = (query) => {
-        fetch('http://localhost:5000/search', {  // Update the URL to point to your Flask server
+        fetch('http://209.38.130.148:5000/search', {  // Update the URL to point to your Flask server
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
